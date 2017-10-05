68ebfc2e2da64d9931097598df388ea18968535f
Jordi Farré <jordi.farre@mango.com>
2017-10-05 20:55:32 +0200

    migrate role creation to python script

--- deploy.py
+++ deploy.py
@@ -1,4 +1,25 @@
-import sys
+import boto3
+
+client = boto3.client("iam")
+
+def createRole(roleName):
+    try:
+        role = client.get_role(RoleName = roleName)
+        return role["Role"]["Arn"]
+    except client.exceptions.NoSuchEntityException:
+        with open("assumeRolePolicyDocument.json", "r") as policy_file:
+            policy = policy_file.read()
+            role = client.create_role(RoleName=roleName, AssumeRolePolicyDocument=policy)
+            return role["Role"]["Arn"]
+
+def putRolePolicy(roleName, policyName, policyFileName):
+    with open(policyFileName, "r") as policyFile:
+        policyContent = policyFile.read()
+        client.put_role_policy(RoleName=roleName, PolicyName=policyName, PolicyDocument=policyContent)
+
 
 if __name__ == '__main__':
-    print(sys.version)
+    roleName = ""
+    roleArn = createRole(roleName = roleName)
+    putRolePolicy(roleName, "s3", "s3PolicyFile.json")
+    putRolePolicy(roleName, "log", "logPolicyFile.json")

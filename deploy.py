f82a9954915ef012a5493f5122f06397a2cf50d7
Jordi Farré <jordi.farre@mango.com>
2017-10-06 01:12:46 +0200

    upload to s3

--- deploy.py
+++ deploy.py
@@ -2,31 +2,36 @@ import boto3
 import pip
 import shutil
 
-client = boto3.client("iam")
+iam = boto3.client("iam")
+s3 = boto3.resource('s3')
 
-def createRole(roleName):
+def create_role(role_name):
     try:
-        role = client.get_role(RoleName = roleName)
+        role = iam.get_role(RoleName = role_name)
         return role["Role"]["Arn"]
-    except client.exceptions.NoSuchEntityException:
+    except iam.exceptions.NoSuchEntityException:
         with open("assumeRolePolicyDocument.json", "r") as policy_file:
             policy = policy_file.read()
-            role = client.create_role(RoleName=roleName, AssumeRolePolicyDocument=policy)
+            role = iam.create_role(RoleName=role_name, AssumeRolePolicyDocument=policy)
             return role["Role"]["Arn"]
 
-def putRolePolicy(roleName, policyName, policyFileName):
-    with open(policyFileName, "r") as policyFile:
+def put_role_policy(role_name, policy_name, policy_file_name):
+    with open(policy_file_name, "r") as policyFile:
         policyContent = policyFile.read()
-        client.put_role_policy(RoleName=roleName, PolicyName=policyName, PolicyDocument=policyContent)
+        iam.put_role_policy(RoleName=role_name, PolicyName=policy_name, PolicyDocument=policyContent)
 
-def createPackage(packageName):
+def create_package(package_name):
     pip.main(["install", "-r", "requirements.txt", "-t", "."])
-    shutil.make_archive(packageName, 'zip', ".")
+    shutil.make_archive(package_name, 'zip', ".")
+
+def upload_to_s3(source, bucket, key):
+    s3.meta.client.upload_file(File=source, Bucket=bucket, Key=key)
 
 
 if __name__ == '__main__':
     roleName = "daily-tee-notifier-lambda-role"
-    roleArn = createRole(roleName = roleName)
-    putRolePolicy(roleName, "s3", "s3PolicyFile.json")
-    putRolePolicy(roleName, "log", "logPolicyFile.json")
-    createPackage("daily-tee-notifier")
+    roleArn = create_role(role_name= roleName)
+    put_role_policy(roleName, "s3", "s3PolicyFile.json")
+    put_role_policy(roleName, "log", "logPolicyFile.json")
+    create_package("daily-tee-notifier")
+    upload_to_s3("daily-tee-notifier.zip", "daily-tee-finder-deployment", "daily-tee-notifier.zip")

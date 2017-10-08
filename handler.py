b73f4810da849ebbc5914cdec17c1a4419e6e09a
Jordi Farré <jordi.farre@mango.com>
2017-10-08 21:11:18 +0200

    Revert "removing old deployment script"
    This reverts commit 23bf5d9c66d2390a1d2c06ebda422e0e7d315b36.

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import json
 
 
-def handle(event, context):
+def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event
